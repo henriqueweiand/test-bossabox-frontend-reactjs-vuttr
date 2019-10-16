de63ecb6ff13b8e6d4de1abdd3de84a921907a19
Henrique Weiand <henriqueweiand@gmail.com>
2019-10-16 20:48:48 -0300

    add validation on form

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 
 import { Container, Input as InputStyle, Label } from './styles';
 
-export default function Input({label, name, height, as, onChange}) {
+export default function Input({label, placeholder, name, height, as, onChange}) {
   return (
     <Container htmlFor={`input-${name}`}>
       {label && <Label>{label}</Label>}
       <InputStyle
+        placeholder={placeholder}
         name={name}
         onChange={onChange}
         height={height}
@@ -19,10 +20,12 @@ export default function Input({label, name, height, as, onChange}) {
 }
 
 Input.defaultProps = {
-  as: 'input'
+  as: 'input',
+  placeholder: '',
 };
 
 Input.propTypes = {
+  placeholder: PropTypes.string,
   label: PropTypes.string,
   name: PropTypes.string.isRequired,
   height: PropTypes.string,
--- src/pages/Main/components/Create/index.js
+++ src/pages/Main/components/Create/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import * as Yup from 'yup';
 
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
@@ -9,11 +10,24 @@ import { Container, Header, Body, Footer } from './styles';
 export default function Create({open, ...rest}) {
   const [form, setForm] = useState({});
 
-  function submit() {
-    rest.onSave({
-      ...form,
-      tags: form.tags.split(' '),
-    });
+  const schema = Yup.object().shape({
+    title: Yup.string().required('Title required'),
+    link: Yup.string().required('Link required'),
+    description: Yup.string().required('Description required'),
+    tags: Yup.string().required('Tags required'),
+  });
+
+  async function handleSubmit(data) {
+    const validForm = await schema.isValid({...form});
+
+    if (validForm) {
+      rest.onSave({
+        ...form,
+        tags: form.tags.split(' '),
+      });
+    } else {
+      alert('All field are requireds');
+    }
   }
 
   function onChange(evt) {
@@ -30,25 +44,32 @@ export default function Create({open, ...rest}) {
           + Add new tool
         </Header>
 
-        <Body>
+        <Body context={{form}} schema={schema} onSubmit={handleSubmit}>
 
           <Input label="Tool title" value="" name="title" onChange={onChange} />
           <Input label="Tool Link" value="" name="link" onChange={onChange} />
           <Input
             height="10rem"
             as="textarea"
+            multiline
             label="Tool Description"
             value=""
             name="description"
             onChange={onChange}
           />
-          <Input label="Tags" value="" name="tags" onChange={onChange} />
+          <Input
+            placeholder="Enter tags with space"
+            label="Tags"
+            value=""
+            name="tags"
+            onChange={onChange}
+          />
 
         </Body>
 
         <Footer>
           <Button border onClick={() => rest.onClose(false)} text="Cancel" />
-          <Button border text="Add tool" onClick={() => submit()} />
+          <Button border type="submit" text="Add tool" onClick={() => handleSubmit()} />
         </Footer>
       </Container>
     </Modal>
